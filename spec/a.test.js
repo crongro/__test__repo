1844f433637531a21f3f318b8dee68f644e14bbe
jisuyoun <nigayoun@gmail.com>
2019-09-23 18:05:50 +0900

    feature. test 환경구축

---
+++ spec/a.test.js
@@ -0,0 +1,9 @@
+const _ = require("../src/a.js");
+
+describe('module a test', () => {  
+  test('add numbers', () => {
+    const number = 100;
+    const result = _.add(number);
+    expect(result).toEqual(200);
+  })
+})
